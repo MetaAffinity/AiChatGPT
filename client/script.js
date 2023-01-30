235908816e6734e3d57f18a3714ecc5bb4856e03
MetaAffinity <metaaffinity@gmail.com>
2023-01-30 16:28:55 +0500

    script file

--- client/script.js
+++ client/script.js
@@ -88,8 +88,8 @@ const handleSubmit = async (e) => {
 
 
 
-    const response = await fetch('http://localhost:5000/', {
-    //const response = await fetch('https://metaaffinityaichat.onrender.com', {
+    //const response = await fetch('http://localhost:5000/', {
+    const response = await fetch('https://metaaffinityaichat.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
