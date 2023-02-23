18a07bac2ef45d5c3f55744e74eda29724e38583
MetaAffinity <metaaffinity@gmail.com>
2023-02-23 23:14:29 +0500

    update

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ let messageCount = parseInt(localStorage.getItem('messageCount')) || 0;
 // Disable the form if the user has exceeded the message limit
 function checkMessageLimit() {
   if (messageCount >= 3) {
-    form.querySelector('submit[type="button"]').disabled = true;
+    form.querySelector('button[type="submit"]').disabled = true;
     document.getElementById("note-textarea").disabled = true;
     const errorDiv = document.querySelector('.message-limit-exceeded');
         errorDiv.style.display = 'block';
